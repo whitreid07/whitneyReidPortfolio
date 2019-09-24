9dd963005562777cbf205c5a5a38c0fe4c901674
Whitney Reid <whitreid@hotmail.com>
2019-09-23 22:19:47 -0400

    added hamburger menu 🤩

--- js/script.js
+++ js/script.js
@@ -6,4 +6,40 @@ $(document).ready(function () {
       scrollTop: $('.headerContainer').offset().top + $('.headerContainer').height()
     }, 800);
   });
+  $('.arrowUpContainer').click(function (event) {
+    event.preventDefault();
+    console.log('clicked!');
+    $('html, body').animate({ scrollTop: 0 }, 800);
+  });
+
+  $(function () {
+    $(".menu").click(function () {
+      if ($("#navigation").hasClass("hidden")) {
+        $("#navigation").attr("class", "visible animated slideInLeft");
+      } else {
+        $("#navigation").attr("class", "hidden animated slideOutLeft");
+      }
+      $(this).toggleClass("open");
+    });
+
+    $("#navigation").click(function () {
+      if ($("#navigation").hasClass("visible")) {
+        $(".menu").toggleClass("open");
+      } else {
+      }
+      $(this).attr("class", "slideOutLeft hidden");
+    });
+
+    $(document).keyup(function (e) {
+      if (e.keyCode == 27) {
+        if ($("#navigation").hasClass("visible")) {
+          $(".menu").toggleClass("open");
+        } else {
+        }
+        $("#navigation").attr("class", "slideOutLeft hidden");
+      }
+    });
+  });
+
 })
+
