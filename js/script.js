cdac58c394e92ef88e369feb65728350d98a7420
Whitney Reid <whitreid@hotmail.com>
2019-09-24 23:13:26 -0400

    added updates like mockups and styling 😎

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
+  //HAMBURGER MENU
+  $('.hamburger').on('click', function () {
+    // toggle class show on the menu
+    $('.menu').toggleClass('show');
+  });
+
+  $('.exit-responsive').on('click', function () {
+    // toggle class show on the menu
+    $('.menu').toggleClass('show');
+  });
+
   //TO SCROLL TO NEAREST SECTION
   $('.arrowContainer').click(function (event) {
     event.preventDefault();
@@ -13,7 +24,7 @@ $(document).ready(function () {
   $('.arrowUpContainer').click(function (event) {
     event.preventDefault();
     console.log('clicked!');
-    $('html, body').animate({ scrollTop: 0 }, 800);
+    $('html, body').animate({ scrollTop: 0 }, 1500);
   });
 
   //SCROLL TO PROJECTS SECTION
@@ -22,16 +33,34 @@ $(document).ready(function () {
     console.log('clicked!');
     $('html, body').animate({
       scrollTop: $('.headerContainer').offset().top + $('.headerContainer').height()
-    }, 800);
+    }, 2000);
   });
 
-  //SCROLL TO CONTACT SECTION
+  //SCROLL TO SKILLS SECTION
   $('.list2').click(function (event) {
     event.preventDefault();
     console.log('clicked!');
     $('html, body').animate({
+      scrollTop: $('.skillsToolkit').offset().top + $('#skills').height()
+    }, 1000);
+  });
+
+  //SCROLL TO CONTACT SECTION FROM HEADER LINK
+  $('.connect').click(function (event) {
+    event.preventDefault();
+    console.log('clicked!');
+    $('html, body').animate({
       scrollTop: $('.contactFormContainer').offset().top + $('.contactFormContainer').height()
-    }, 800);
+    }, 2000);
+  });
+
+  //SCROLL TO CONTACT SECTION
+  $('.list3').click(function (event) {
+    event.preventDefault();
+    console.log('clicked!');
+    $('html, body').animate({
+      scrollTop: $('.contactFormContainer').offset().top + $('.contactFormContainer').height()
+    }, 2000);
   });
 })
 
